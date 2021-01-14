696ce84ce31585842c751dcaa55923e573d3b51f
Greycoat21 <matthewfmcandrews@gmail.com>
2021-01-14 17:59:09 -0600

    Update Build_10.4.bat
    simplify

--- Utils/Build_10.4.bat
+++ Utils/Build_10.4.bat
@@ -1,6 +1,2 @@
-rem Verify and change to the path for your rsvars.bat as needed
-rem The default for 10.4 Sydney is C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat
-call "C:\Delphi10.4\bin\rsvars.bat"
-
-rem second parameter is .dproj filename with relative path within work folder
+set DSUnattendedBuild
 "C:\program files (x86)\embarcadero\studio\20.0\bin\bds.exe" -ns %1 -b
